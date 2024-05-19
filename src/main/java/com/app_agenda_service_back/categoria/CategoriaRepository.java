5269b5e2cac65480c2e9ae366d1b08a883545aa2
isamuneretto <isamuneretto@gmail.com>
2024-05-19 13:56:51 -0300

    created Repository Categoria, Endereco e Usuario

---
+++ src/main/java/com/app_agenda_service_back/categoria/CategoriaRepository.java
@@ -0,0 +1,6 @@
+package com.app_agenda_service_back.categoria;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<CategoriaEntity,Long> {
+}
---
+++ src/main/java/com/app_agenda_service_back/usuario/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package com.app_agenda_service_back.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity,Long> {
+}
